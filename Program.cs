a9e4b6e8a278689ef12de0dafa08f84902f805fe
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-23 15:08:26 +0200

    Completati esercizi base

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] PreferiteFoods = { "pizza", "bbq", "lasagne", "tiramisu", "pasta", "fried fish", "arancino" };
+
+Console.WriteLine($"Il totale dei miei cibi preferiti è: {PreferiteFoods.Length}");
+
+for (int i = 0; i < PreferiteFoods.Length; i++)
+{
+    Console.WriteLine($"Alla posizione n.{i+1} {PreferiteFoods[i]}");
+}
+
+Console.WriteLine($"Il mio cibo preferito è: {PreferiteFoods[0]}");
+
+Console.WriteLine($"Il cibo meno preferito è: {PreferiteFoods[PreferiteFoods.Length-1]}");
+
+
+int MediaFoods = PreferiteFoods.Length/2;
+
+if (MediaFoods/2 == 0)
+{
+    Console.WriteLine(PreferiteFoods[MediaFoods - 1]);
+}
